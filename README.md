5cc5c34a4a0271443cdf7542bafe1cfea91c1da4
Martin Gyselinck <martin.gyselinck@student.uclouvain.be>
2023-02-09 16:57:39 +0100

    Rick

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # AI-LINFO1361
+This repository contains the Pontus player created by Quentin Prieels and Martin Gyselinck.
+We don't have to forget than we already lose before it started.
+AH MAIS Y'A DES POINTS BONUS GOGOGO
+PS : Quentin tu bois
---
+++ main.py
@@ -0,0 +1,9 @@
+# rickroll the user by opening the youtube video in the default browser
+# and then playing the audio in the background
+
+import webbrowser
+import time
+import os
+
+# open the youtube video in the default browser
+webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
