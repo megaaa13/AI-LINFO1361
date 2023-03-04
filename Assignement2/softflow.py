0492d85e649cbe363d0a43bd478b3911d9712ccd
Martin Gyselinck <martin.gyselinck02@gmail.com>
2023-03-04 22:24:05 +0100

    Manhattan distance + shortest path

--- Assignement2/softflow.py
+++ Assignement2/softflow.py
@@ -24,12 +24,16 @@ class SoftFlow(Problem):
     def result(self, state, action):
         #print(action)
         new_grid = list(state.grid)
-        new_grid[state.positions[action[0]][0]] = list(new_grid[state.positions[action[0]][0]])
-        new_grid[state.positions[action[0]][0]][state.positions[action[0]][1]] = state.positions[action[0]][2]
-        new_grid[state.positions[action[0]][0]] = tuple(new_grid[state.positions[action[0]][0]])
-        new_grid[action[1][0]] = list(new_grid[action[1][0]])
-        new_grid[action[1][0]][action[1][1]] = action[0]
-        new_grid[action[1][0]] = tuple(new_grid[action[1][0]])
+        # replacing the letter by the pipe
+        new_grid[state.positions[action[0]][0]] = new_grid[state.positions[action[0]][0]][0:state.positions[action[0]][1]] + tuple(state.positions[action[0]][2]) + new_grid[state.positions[action[0]][0]][state.positions[action[0]][1] + 1:]
+        # placing the letter
+        new_grid[action[1][0]] = new_grid[action[1][0]][0:action[1][1]] + tuple(action[0]) + new_grid[action[1][0]][action[1][1] + 1:]
+        #new_grid[state.positions[action[0]][0]] = list(new_grid[state.positions[action[0]][0]])
+        #new_grid[state.positions[action[0]][0]][state.positions[action[0]][1]] = state.positions[action[0]][2]
+        #new_grid[state.positions[action[0]][0]] = tuple(new_grid[state.positions[action[0]][0]])
+        #new_grid[action[1][0]] = list(new_grid[action[1][0]])
+        #new_grid[action[1][0]][action[1][1]] = action[0]
+        #new_grid[action[1][0]] = tuple(new_grid[action[1][0]])
         new_state = State(tuple(new_grid), state.positions, state.goal_reached)
         new_state.positions[action[0]] = action[1]
         #print(new_state)
@@ -40,25 +44,31 @@ class SoftFlow(Problem):
         for k in state.positions.keys():
             if (abs(state.positions[k][0] - self.goal[k][0]) + abs(state.positions[k][1] - self.goal[k][1]) == 1) and state.goal_reached[int(state.positions[k][2])] == False:
                 state.goal_reached[int(state.positions[k][2])] = True
-                print("Goal reached: ", k)
+                #print("Goal reached: ", k)
                 #modify letters in the grid
                 new_grid = list(state.grid)
-                new_grid[state.positions[k][0]] = list(new_grid[state.positions[k][0]])
-                new_grid[state.positions[k][0]][state.positions[k][1]] = state.positions[k][2]
-                new_grid[state.positions[k][0]] = tuple(new_grid[state.positions[k][0]])
+                new_grid[state.positions[k][0]] = new_grid[state.positions[k][0]][0:state.positions[k][1]] + tuple(state.positions[k][2]) + new_grid[state.positions[k][0]][state.positions[k][1] + 1:]
+                #new_grid[state.positions[k][0]] = list(new_grid[state.positions[k][0]])
+                #new_grid[state.positions[k][0]][state.positions[k][1]] = state.positions[k][2]
+                #new_grid[state.positions[k][0]] = tuple(new_grid[state.positions[k][0]])
                 state.grid = tuple(new_grid)
         #check if all goals are reached
         for k in state.goal_reached:
             if k == False:
                 return False
-        print("Goals: ", self.goal)
+        #print("Goals: ", self.goal)
         return True
 
     def h(self, node):
         h = 0
-        for i in node.state.goal_reached:
+        for k in node.state.positions.keys():
+            if node.state.goal_reached[int(node.state.positions[k][2])] == False:
+                h += abs(node.state.positions[k][0] - self.goal[k][0]) + abs(node.state.positions[k][1] - self.goal[k][1]) # manhattan distance
+        #print(h)
+        for i in node.state.goal_reached: # prefer to choose path with already good solutions
             if i == False:
                 h += 100
+        h += node.depth # optimal solution is the shortest one
         return h
         
 
@@ -67,7 +77,7 @@ class SoftFlow(Problem):
             lines = f.readlines()
             
         state = State.from_string(''.join(lines))
-        print(state)
+        #print(state)
         return SoftFlow(state)
     
     def get_goal(self, state):
@@ -79,7 +89,7 @@ class SoftFlow(Problem):
                 for j in range(state.nbc):
                     if state.grid[i][j] == state.positions[k][2]:
                         goal[k[0]] = (i, j) # False means that the goal is not reached
-        print("Goals : ", goal)
+        #print("Goals : ", goal)
         return goal
 
 
@@ -96,18 +106,19 @@ class State:
         self.grid = grid
         self.positions = positions.copy() if positions else self.get_positions(self)
         self.goal_reached = goal.copy() if goal else [False for _ in range(len(self.positions))]
+        self.hash = hash(self.grid)
         
     def __str__(self):
         return '\n'.join(''.join(row) for row in self.grid)
 
     def __eq__(self, other_state):
-        return self.grid == other_state.grid
+        return self.hash == other_state.hash
 
     def __hash__(self):
-        return hash((self.grid))
+        return hash(self.grid)
     
     def __lt__(self, other):
-        return hash(self) < hash(other)
+        return self.hash < other.hash
 
     def from_string(string):
         lines = string.strip().splitlines()
@@ -129,7 +140,7 @@ class State:
                 if idx != None:
                     positions[state.grid[i][j]] = (i, j, str(idx))
                     idx += 1
-        print("Positions : ", positions)
+        #print("Positions : ", positions)
         return positions
 
 
@@ -140,22 +151,23 @@ class State:
 #####################
 # Launch the search #
 #####################
-
+#import time
 problem = SoftFlow.load(sys.argv[1])
-
-node = astar_search(problem)
-
+#start_timer = time.perf_counter()
+node = astar_search(problem, display=False)
+#end_timer = time.perf_counter()
 # example of print
 path = node.path()
 
 print('Number of moves: ', str(node.depth))
 for n in path:
-    if (n.depth == 0):
+    """if (n.depth == 0):
         print('initial state')
     elif (n.depth == node.depth):
         print('goal state')
     else:
-        print('state', str(n.depth))
+        print('state', str(n.depth))"""
     print(n.state)  # assuming that the _str_ function of state outputs the correct format
     print()
-
+#print("* Execution time:\t", str(end_timer - start_timer))
+#print('Number of moves: ', str(node.depth))
