daaafae7e9bcf94c4a30b4ad2171dd9f7ecbedab
Quentin Prieels <quentin.prieels@gmail.com>
2023-03-22 18:46:40 +0100

    Basic files for Assignment3

---
+++ Assignement3/basic_agent.py
@@ -0,0 +1,37 @@
+
+# TODO: renformat the imports for inginious
+from pontu_game.agent import AlphaBetaAgent
+import pontu_game.minimax
+
+
+class MyAgent(AlphaBetaAgent):
+
+  """
+  This is the skeleton of an agent to play the Tak game.
+  """
+  def get_action(self, state, last_action, time_left):
+    self.last_action = last_action
+    self.time_left = time_left
+    return minimax.search(state, self)
+
+  """
+  The successors function must return (or yield) a list of
+  pairs (a, s) in which a is the action played to reach the
+  state s.
+  """
+  def successors(self, state):
+    pass
+
+  """
+  The cutoff function returns true if the alpha-beta/minimax
+  search has to stop and false otherwise.
+  """
+  def cutoff(self, state, depth):
+    pass
+
+  """
+  The evaluate function must return an integer value
+  representing the utility function of the board.
+  """
+  def evaluate(self, state):
+    pass
