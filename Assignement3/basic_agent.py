763e655f49391414eb824790441b8a45e4129dbc
Martin Gyselinck <martin.gyselinck02@gmail.com>
2023-03-26 11:46:10 +0200

    Working basic agent

--- Assignement3/basic_agent.py
+++ Assignement3/basic_agent.py
@@ -1,7 +1,7 @@
-
 # TODO: renformat the imports for inginious
-from pontu_game.agent import AlphaBetaAgent
-import pontu_game.minimax as minimax
+from agent import AlphaBetaAgent
+import minimax as minimax
+import pontu_state as pontu_state
 
 
 class MyAgent(AlphaBetaAgent):
@@ -19,22 +19,35 @@ class MyAgent(AlphaBetaAgent):
   pairs (a, s) in which a is the action played to reach the
   state s.
   """
-  def successors(self, state):
+  def successors(self, state: pontu_state.PontuState):
     # Seems to be to easy... Have I missed something?
     actions = state.get_current_player_actions()
     for action in actions:
-        yield (action, state.copy().result(action))
+        new_state = state.copy()
+        new_state.apply_action(action)
+        yield (action, new_state)
 
   """
   The cutoff function returns true if the alpha-beta/minimax
   search has to stop and false otherwise.
   """
   def cutoff(self, state, depth):
-    pass
+    if state.game_over():
+        return True
+    if depth == 1:
+        return True
+    return False
 
   """
   The evaluate function must return an integer value
   representing the utility function of the board.
   """
-  def evaluate(self, state):
-    pass
+  def evaluate(self, state: pontu_state.PontuState):
+    nb_bridges = 0
+    for i in range(0, state.size - 2):
+        for bridge in state.adj_bridges(1 - self.id, i).values():
+            if bridge:
+                nb_bridges += 1
+    #print("Evaluate : ", 12 - nb_bridges)    
+    return (state.size - 2) * 4 - nb_bridges
+     
