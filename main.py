154c74f616bc5108bf1dbe8ae20b40a4fa0a2582
Martin Gyselinck <martin.gyselinck02@gmail.com>
2023-08-18 15:52:07 +0200

    New Branch for demonstration

--- main.py
+++
@@ -1,9 +0,0 @@
-# rickroll the user by opening the youtube video in the default browser
-# and then playing the audio in the background
-
-import webbrowser
-import time
-import os
-
-# open the youtube video in the default browser
-webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
